f48ecfff7aaeeb8efa4a31ddf9cd5e38c505e83a
Yash Vardhan <yashvardhan513@gmail.com>
2021-06-07 22:09:45 +0530

    Updates

---
+++ precache-manifest.b4086ae76345f9d5615753d0df36f993.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab987c4bd2a9b1bc9f5cd8eb34c4b0d",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "d2a9ea6b838a7cd61833",
+    "url": "/developerFolio/static/css/3.bc85b1b3.chunk.css"
+  },
+  {
+    "revision": "9fec42f96a486db60f90",
+    "url": "/developerFolio/static/css/4.5c57288c.chunk.css"
+  },
+  {
+    "revision": "0be82711f0814b79e367",
+    "url": "/developerFolio/static/css/main.4b5bc672.chunk.css"
+  },
+  {
+    "revision": "4514e786a79b53413a35",
+    "url": "/developerFolio/static/js/2.21e0061b.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/developerFolio/static/js/2.21e0061b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2a9ea6b838a7cd61833",
+    "url": "/developerFolio/static/js/3.7d26c151.chunk.js"
+  },
+  {
+    "revision": "9fec42f96a486db60f90",
+    "url": "/developerFolio/static/js/4.3e19ea36.chunk.js"
+  },
+  {
+    "revision": "0be82711f0814b79e367",
+    "url": "/developerFolio/static/js/main.803e1153.chunk.js"
+  },
+  {
+    "revision": "1bc4c65d214382b2eaf7",
+    "url": "/developerFolio/static/js/runtime-main.28cd76cf.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/developerFolio/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "644cc984efdd52064812955073272b50",
+    "url": "/developerFolio/static/media/ISM_logo.644cc984.png"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/developerFolio/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "cde7daf2297b38128f4032d05efef7d4",
+    "url": "/developerFolio/static/media/acm-logo.cde7daf2.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/developerFolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/developerFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8d8c584b857f474e61a6794b09e481d2",
+    "url": "/developerFolio/static/media/hack-logo.8d8c584b.jpg"
+  },
+  {
+    "revision": "d226a9a456a4d3ce506744c183ca4fc1",
+    "url": "/developerFolio/static/media/layer-logo.d226a9a4.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/developerFolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "a3fcb02714c861d7e66a03680021cfc3",
+    "url": "/developerFolio/static/media/nighthawk-logo.a3fcb027.svg"
+  },
+  {
+    "revision": "debf3d19c0c9d0a129e1d93bfb9b9563",
+    "url": "/developerFolio/static/media/overflow-logo.debf3d19.jpeg"
+  },
+  {
+    "revision": "3f29a174a2a0196036ba2a2a3d0e8159",
+    "url": "/developerFolio/static/media/score-logo.3f29a174.jpeg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/developerFolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
